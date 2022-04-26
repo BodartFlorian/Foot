7ab09b6ba8c0bb953f947e8c69a3a05d6881a81a
FlorianBodart <bodart.florian@outlook.fr>
2022-04-26 18:07:55 +0200

    version 0.1.0 resolution bug image

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let reset = document.querySelector("#reset");
 
 reset.onclick = function swap(cssFile) {
         let link = document.getElementById("change-style");
-        link.href = ('css/style.css');
+        link.href = ('../css/style.css');
 }
 
 let position1 = document.querySelector("#position1");
@@ -10,10 +10,10 @@ let position1 = document.querySelector("#position1");
 position1.onclick = function swap(cssFile) {
     if (window.matchMedia("(max-width: 767px)").matches){
         let link = document.getElementById("change-style");
-        link.href = ('css/littlePosition1.css');
+        link.href = ('../css/littlePosition1.css');
     }
     else {
         let link = document.getElementById("change-style");
-        link.href = ('css/position1.css');
+        link.href = ('../css/position1.css');
     }
 }
