26ab1199f6e4bf03cb98f0464777116a015cc110
FlorianBodart <bodart.florian@outlook.fr>
2022-04-26 18:04:07 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let reset = document.querySelector("#reset");
+
+reset.onclick = function swap(cssFile) {
+        let link = document.getElementById("change-style");
+        link.href = ('css/style.css');
+}
+
+let position1 = document.querySelector("#position1");
+
+position1.onclick = function swap(cssFile) {
+    if (window.matchMedia("(max-width: 767px)").matches){
+        let link = document.getElementById("change-style");
+        link.href = ('css/littlePosition1.css');
+    }
+    else {
+        let link = document.getElementById("change-style");
+        link.href = ('css/position1.css');
+    }
+}
